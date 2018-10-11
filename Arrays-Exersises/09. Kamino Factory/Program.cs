437394fda43a0714caba7f5df01c224dff7417c6
StackSmack007 <n_hristov007@abv.bg>
2018-10-11 21:19:33 +0300

    Added the * tasks

--- Arrays-Exersises/09. Kamino Factory/Program.cs
+++ Arrays-Exersises/09. Kamino Factory/Program.cs
@@ -4,16 +4,63 @@ class Program
 {
     static void Main()
     {
-        int[] array = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-        int magicNumber = int.Parse(Console.ReadLine());
-        for (int i = 0; i < array.Length - 1; i++)
+        int number = int.Parse(Console.ReadLine());
+        int[] result = new int[number];
+        int bestStartIndex = number - 1;
+        int bestSum = 0;
+        int bestLongestSequence = 0;
+        int bestSequenceNumber = 1;
+        int sequenceNumber = 0;
+        while (true)
         {
-            for (int j = i + 1; j < array.Length; j++)
+            sequenceNumber++;
+            string input = Console.ReadLine();
+            if (input == "Clone them!")
             {
-                if (array[i] + array[j] == magicNumber)
+                Console.WriteLine("Best DNA sample {0} with sum: {1}.", bestSequenceNumber, bestSum);
+                Console.WriteLine(string.Join(" ", result));
+                return;
+            }
+            int[] inputArr = input
+                .Split(new char[] { '!' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int currentStartIndex = number - 1;
+            int currentSum = inputArr.Sum();
+            int currentLongestSequence = 0;
+            for (int i = 0; i < number; i++)
+            {
+                if (inputArr[i] == 1)
                 {
-                    Console.WriteLine(array[i] + " " + array[j]);
+                    int StartLocal = i;
+                    int LenghtLocal = 1;
+                    for (int j = i + 1; j < number; j++)
+                    {
+                        if (inputArr[j] == 1)//не сме ударили нула и искаме още да расте дължината на събстринга
+                        {
+                            LenghtLocal++;
+                        }
+                        if (inputArr[j] == 0 || j == number - 1)//ударили сме нулата/края на масива и трябва или да запишем това ако е по-добро от домомента за този ред или да го подминем след което брейквам вътрешния цикъл
+                        {
+                            if (LenghtLocal > currentLongestSequence)
+                            {
+                                currentLongestSequence = LenghtLocal;
+                                currentStartIndex = StartLocal;
+                            }
+                            break;
+                        }
+                    }
                 }
+            }
+            if (currentLongestSequence > bestLongestSequence ||
+               (currentLongestSequence == bestLongestSequence & currentStartIndex < bestStartIndex) ||
+               (currentLongestSequence == bestLongestSequence & currentStartIndex == bestStartIndex & currentSum > bestSum))
+            {
+                result = inputArr;
+                bestStartIndex = currentStartIndex;
+                bestSum = currentSum;
+                bestLongestSequence = currentLongestSequence;
+                bestSequenceNumber = sequenceNumber;
             }
         }
     }
---
+++ Arrays-Exersises/10. LadyBugs/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        int[] field = new int[int.Parse(Console.ReadLine())];
+        int[] ladybugIndexes = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        for (int i = 0; i < field.Length; i++)
+        {
+            if (ladybugIndexes.Contains(i))
+            {
+                field[i] = 1;
+            }
+            else
+            {
+                field[i] = 0;
+            }
+        }
+        while (true)
+        {
+            string[] command = Console.ReadLine().Split(new char[] { ' ' });
+            if (command[0] == "end")
+            {
+                Console.WriteLine(string.Join(" ", field));
+                return;
+            }
+            string destination = command[1];
+            int startIndex = int.Parse(command[0]);
+            int distance = int.Parse(command[2]);
+            int endIndex = startIndex + (destination == "right" ? distance : -distance);
+            if (startIndex < 0 || startIndex > field.Length - 1)
+            {
+                continue;
+            }
+            else if (field[startIndex] == 0)
+            {
+                continue;
+            }
+            field[startIndex] = 0;//we have a liftOff!
+            while (endIndex >= 0 & endIndex < field.Length)//findSpaceForLanding
+            {
+                if (field[endIndex] == 1)
+                {
+                    if (destination == "right")
+                    {
+                        endIndex += distance;
+                    }
+                    else
+                    {
+                        endIndex -= distance;
+                    }
+                }
+                else
+                {
+                    field[endIndex] = 1;
+                    break;
+                }
+            }
+        }
+    }
+}
