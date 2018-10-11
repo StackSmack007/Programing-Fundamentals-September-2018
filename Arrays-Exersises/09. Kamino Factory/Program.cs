3536ecd9ebd498224af9992cc26458d7e470205d
StackSmack007 <n_hristov007@abv.bg>
2018-10-11 19:14:25 +0300

    Kammino

---
+++ Arrays-Exersises/09. Kamino Factory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        int[] array = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int magicNumber = int.Parse(Console.ReadLine());
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[i] + array[j] == magicNumber)
+                {
+                    Console.WriteLine(array[i] + " " + array[j]);
+                }
+            }
+        }
+    }
+}
